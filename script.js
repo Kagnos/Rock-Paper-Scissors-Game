ad05847ea28c1f8127a4d65c47f91c248f30a2c8
Kagnos <kagcodes@gmail.com>
2025-12-31 12:36:03 -0600

    Finish computer choice and human choice functions
    Computer choice can be called as a function
     and is represented as a variable.
    Human choice accepts any case variant of the string.
    Human choice can be called as a function and is
    represented as a variable.

--- script.js
+++ script.js
@@ -1,11 +1,32 @@
+let computerChoice;
+let humanChoice;
+
 function getComputerChoice() {
-    return Math.floor(Math.random() * 3);
+    return computerChoice = Math.floor(Math.random() * 3);
 }
 
-console.log(getComputerChoice());
+getComputerChoice();
+console.log(computerChoice);
 
 function getHumanChoice() {
-    return "Rock, paper, or scissors?";
+    humanChoice = prompt("Rock, paper, or scissors?");
+    if (humanChoice.toLowerCase() === "rock") {
+        return humanChoice = 0;
+    } else if (humanChoice.toLowerCase() === "paper") {
+        return humanChoice = 1;
+    } else if (humanChoice.toLowerCase() === "scissors" || humanChoice.toLowerCase() === "scissor") {
+        return humanChoice = 2;
+    } else {
+        console.log("player can't spell lmao");
+    }
 }
 
-prompt(getHumanChoice());
+getHumanChoice();
+console.log(humanChoice);
+
+let computerScore = 0;
+let humanScore = 0;
+
+function playRound(computerChoice, humanChoice) {
+
+}
