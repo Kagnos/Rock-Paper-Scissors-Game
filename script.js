7795f675875e4b1652e68e2627f0eedd9ff7fb63
Kagnos <kagcodes@gmail.com>
2026-01-01 08:26:51 -0600

    Polish playRound function and outline playGame
    Nest getHumanChoice and getComputerChoice inside playRound.
    getHumanChoice only calls getComputerChoice if given a valid input.
    playRound updates humanScore and computerScore if given a valid input.
    Create playGame outline.

--- script.js
+++ script.js
@@ -1,40 +1,89 @@
 let computerChoice;
 let humanChoice;
 
-function getComputerChoice() {
-    return computerChoice = Math.floor(Math.random() * 3);
-}
-
-getComputerChoice();
-console.log(computerChoice);
-
-function getHumanChoice() {
-    humanChoice = prompt("Rock, paper, or scissors?");
-    if (humanChoice.toLowerCase() === "rock") {
-        return humanChoice = 0;
-    } else if (humanChoice.toLowerCase() === "paper") {
-        return humanChoice = 1;
-    } else if (humanChoice.toLowerCase() === "scissors" || humanChoice.toLowerCase() === "scissor") {
-        return humanChoice = 2;
-    } else {
-        return humanChoice = "player can't spell lmao";
-    }
-}
-
-getHumanChoice();
-console.log(humanChoice);
-
 let computerScore = 0;
 let humanScore = 0;
 
 function playRound(computerChoice, humanChoice) {
-    if (computerChoice === 0 && humanChoice === 1 || computerChoice === 1 && humanChoice === 2 || computerChoice === 2 && humanChoice === 1) {
-        console.log("You win!");
-    } else if (computerChoice === 0 && humanChoice === 2 || computerChoice === 1 && humanChoice === 0 || computerChoice === 2 && humanChoice === 1) {
-        console.log("You lose!");
+    function getHumanChoice() {
+    humanChoice = prompt("Rock, paper, or scissors?");
+
+    if (humanChoice === null) {
+        console.log(humanChoice = "Player cancelled.");
+        return;
+    } else if (humanChoice.toLowerCase() === "rock") {
+        console.log("Player chooses rock.");
+        getComputerChoice();
+        return humanChoice = 0;
+    } else if (humanChoice.toLowerCase() === "paper") {
+        console.log("Player chooses paper.");
+        getComputerChoice();
+        return humanChoice = 1;
+    } else if (humanChoice.toLowerCase() === "scissors" || humanChoice.toLowerCase() === "scissor") {
+        console.log("Player chooses scissors.");
+        getComputerChoice();
+        return humanChoice = 2;
     } else {
-        console.log("It's a tie");
+        console.log(humanChoice = "Player can't spell.");
+        return;
+    }
+}
+    
+    function getComputerChoice() {
+    computerChoice = Math.floor(Math.random() * 3);
+
+    if (computerChoice === 0) {
+        console.log("Computer chooses rock.");
+        return computerChoice = 0;
+    } else if (computerChoice === 1) {
+        console.log("Computer chooses paper.");
+        return computerChoice = 1;
+    } else if (computerChoice === 2) {
+        console.log("Computer chooses scissors.");
+        return computerChoice = 2;
+    }
+}
+
+    getHumanChoice();
+    
+
+    if (humanChoice === null) {
+        return;
+    } else if (computerChoice === 0 && humanChoice === 1 || computerChoice === 1 && humanChoice === 2 || computerChoice === 2 && humanChoice === 1) {
+        ++ humanScore;
+        console.log("You win!");
+        console.log(`Computer Score: ${computerScore}`);
+        console.log(`Human Score: ${humanScore}`);
+    } else if (computerChoice === 0 && humanChoice === 2 || computerChoice === 1 && humanChoice === 0 || computerChoice === 2 && humanChoice === 1) {
+        ++ computerScore;
+        console.log("You lose!");
+        console.log(`Computer Score: ${computerScore}`);
+        console.log(`Human Score: ${humanScore}`);
+    } else if (computerChoice === humanChoice) {
+        console.log("It's a tie!");
+        console.log(`Computer Score: ${computerScore}`);
+        console.log(`Human Score: ${humanScore}`);
+    } else {
+        return;
     }
 }
 
 playRound(computerChoice, humanChoice);
+
+function playGame (playRound) {
+    playRound(computerChoice, humanChoice);
+    playRound(computerChoice, humanChoice);
+    playRound(computerChoice, humanChoice);
+    playRound(computerChoice, humanChoice);
+    playRound(computerChoice, humanChoice);
+    
+    if (computerScore < humanScore) {
+        console.log(`${humanScore} - ${computerScore} You win the game!`);
+    } else if (computerScore > humanScore) {
+        console.log(`${humanScore} - ${computerScore} You lose the game!`);
+    } else if (computerScore === 0 && humanScore === 0) {
+        console.log(`${humanScore} - ${computerScore} Did you even try?`)
+    } else if (computerScore === humanScore) {
+        console.log(`${humanScore} - ${computerScore} You tied the game!`);
+    }
+}
